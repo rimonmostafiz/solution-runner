e25b225adee656daa9e4dde654aa4df524cd3b8f
Faisal Ahmed <faisal.ahmed@tigeritbd.com>
2016-03-15 08:48:59 +0600

    added partial logic to ApplicantTask

--- src/main/java/com/tigerit/exam/ApplicantTask.java
+++ src/main/java/com/tigerit/exam/ApplicantTask.java
@@ -1,7 +1,18 @@
 package com.tigerit.exam;
 
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author Faisal Ahmed
@@ -21,8 +32,24 @@ public class ApplicantTask implements Runnable {
 
     @Override
     public void run() {
-        // create applicant directory with email address
-        // clone applicant repo from github
+        try {
+            // create applicant directory with email address
+            String applicantRepoLocation = configuration.get(
+                    "repo.root.directory.name") + "/" + applicant.getEmail();
+            logger.debug("Applicant repo directory created");
+            Files.createDirectory(Paths.get(applicantRepoLocation));
+
+
+            // clone applicant repo from github
+            Git git = Git.cloneRepository().setURI(applicant.getRepo())
+                    .setDirectory(new File(applicantRepoLocation)).call();
+            logger.debug("{} repo cloned successfully", applicant.getRepo());
+
+        } catch (Exception e) {
+            logger.debug("Unhandled Runtime Exception");
+            throw new RuntimeException(e);
+        }
+
         // build applicant repo
         // load applicant solution.jar dynamically
         // redirect stream
